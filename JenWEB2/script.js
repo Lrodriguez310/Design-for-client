e3df90cbf469fe4ab83e776279f75c51ff2cfaa3
Luis Rodriguez <105326568+Lrodriguez310@users.noreply.github.com>
2023-03-19 18:10:39 -0400

    Add files via upload

---
+++ JenWEB2/script.js
@@ -0,0 +1,9 @@
+/* typing anime */
+var typed = new Typed(".typing",{
+	strings:["","Facials","Eyelashes","Cat Eyes","Eyelash Tint","Brow fillers","Lash Lift"],
+	typeSpeed:100,
+	BackSpeed:60,
+	loop: true
+	
+})
+
